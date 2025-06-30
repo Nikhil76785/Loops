b333e9a1f406044958b89ef17142ce7567f923f1
Nikhil76785 <valivetinikhil2@gmail.com>
2025-06-30 19:46:25 +0530

    AGE CHECK

---
+++ loop1.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+
+if (age >= 10):
+    if (age >= 20):
+        print(f"Your age is {age} and you are allowed to enter the class.")
+    
+    else:
+        print(f"Your age is {age} and you are not allowed to enter the class.")
+
+else:
+    print(f"Your age is {age} and you are not eligible.")
